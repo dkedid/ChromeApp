388b311e020e3ff2ce9032309c4c4999b2facf7e
dkedid <dkedid@users.noreply.github.com>
2021-02-13 17:03:58 +0900

    LocalStorage

--- JS/src/index.js
+++ JS/src/index.js
@@ -1,28 +1,30 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 const selectValue = document.querySelector("select");
 const h1 = document.querySelector("h1");
 
 const greetingDF = "Where are you from?",
-greetingKR = "안녕하세요",
-greetingGR = "γεια",
-greetingTK = "Selam",
-greetingFL = "Hei"
+  greetingKR = "안녕하세요",
+  greetingGR = "γεια",
+  greetingTK = "Selam",
+  greetingFL = "Hei";
 
 selectValue.addEventListener("input", saveValue);
-function saveValue(){
+function saveValue() {
   let country = selectValue.value;
   localStorage.setItem("Country", country);
   /* Greeting */
   if (country == "default") {
-    h1.innerText = `${greetingDF}! :)`
+    h1.innerText = `${greetingDF}! :)`;
   } else if (country == "Korea") {
-  h1.innerText = `${greetingKR}! :)`
+    h1.innerText = `${greetingKR}! :)`;
   } else if (country == "Greece") {
-  h1.innerText = `${greetingGR}! :)`
+    h1.innerText = `${greetingGR}! :)`;
   } else if (country == "Turkey") {
-  h1.innerText = `${greetingTK}! :)`
+    h1.innerText = `${greetingTK}! :)`;
   } else if (country == "Finland") {
-  h1.innerText = `${greetingFL}! :)`
+    h1.innerText = `${greetingFL}! :)`;
   }
 }
 
@@ -33,19 +35,18 @@ function loadData() {
     //nothing
   } else {
     if (country == "default") {
-      h1.innerText = `${greetingDF}! :)`
+      h1.innerText = `${greetingDF}! :)`;
     } else if (country == "Korea") {
-    h1.innerText = `${greetingKR}! :)`
+      h1.innerText = `${greetingKR}! :)`;
     } else if (country == "Greece") {
-    h1.innerText = `${greetingGR}! :)`
+      h1.innerText = `${greetingGR}! :)`;
     } else if (country == "Turkey") {
-    h1.innerText = `${greetingTK}! :)`
+      h1.innerText = `${greetingTK}! :)`;
     } else if (country == "Finland") {
-    h1.innerText = `${greetingFL}! :)`
+      h1.innerText = `${greetingFL}! :)`;
     }
   }
 }
-
 
 function init() {
   loadData();
